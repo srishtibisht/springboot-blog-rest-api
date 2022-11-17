b4d25397828d83d83f3fb96ffd5272da1f5ac4d5
srishtibisht <45358019+srishtibisht@users.noreply.github.com>
2022-11-17 13:11:16 +0530

    Commit configurations
    First commit

---
+++ src/main/java/com/app/blog/SpringbootBlogAppRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.app.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogAppRestApiApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
