8b027fef61aae8cc5f1454d832126ee578eda4ed
srishtibisht <45358019+srishtibisht@users.noreply.github.com>
2022-11-18 07:56:08 +0530

    Bean classes - Post Request Response

--- src/main/java/com/app/blog/entity/Post.java
+++ src/main/java/com/app/blog/entity/Post.java
@@ -11,31 +11,72 @@ import javax.persistence.UniqueConstraint;
 import lombok.Data;
 import lombok.Setter;
 
-
-
 @Data
 @Entity
-@Table(name="posts", uniqueConstraints = @UniqueConstraint(columnNames = {"title"}))
+@Table(name = "posts", uniqueConstraints = @UniqueConstraint(columnNames = { "title" }))
 public class Post {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 
-	@Column(name = "title",nullable = false)
+	@Column(name = "title", nullable = false)
 	public String title;
-	
-	@Column(name="description",nullable = false)
+
+	@Column(name = "description", nullable = false)
 	private String description;
-	
-	@Column(name="content",nullable = false)
+
+	@Column(name = "content", nullable = false)
 	private String content;
 
+	public Post() {
+		super();
+	}
 
+	public Post(String title, String description, String content) {
+		this.title = title;
+		this.description = description;
+		this.content = content;
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", title=" + title + ", description=" + description + ", content=" + content + "]";
+	}
 	
 	
 }
 
-
+}
--- src/main/java/com/app/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/app/blog/service/impl/PostServiceImpl.java
@@ -7,22 +7,27 @@ import com.app.blog.payload.PostDto;
 import com.app.blog.repository.PostRepository;
 import com.app.blog.service.PostService;
 
-public class PostServiceImpl implements PostService{
-	
+public class PostServiceImpl implements PostService {
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public PostDto createPost(PostDto postDto) {
 		Post post = new Post();
-		
-		
-		
-		PostDto response = new PostDto();
-		
-		
-		return response;
+		post.setTitle(postDto.getTitle());
+		post.setDescription(postDto.getDescription());
+		post.setContent(postDto.getContent());
+
+		Post nPost = postRepository.save(post);
+
+		PostDto postResponse = new PostDto();
+		postResponse.setId(nPost.getId());
+		postResponse.setTitle(nPost.getTitle());
+		postResponse.setDescription(nPost.getDescription());
+		postResponse.setContent(nPost.getContent());
+
+		return postResponse;
 	}
-	
 
 }
